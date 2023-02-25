486d9b2dfbb28ca49d351845fa4ffb6c81d38c63
Pablo Barba <pabbarso181189@gmail.com>
2023-02-25 15:54:47 +0100

    Atributos cuenta añadidos

--- src/model/Cuenta.java
+++ src/model/Cuenta.java
@@ -1,10 +1,16 @@
 package model;
 
 public class Cuenta {
+    //Numero identificador de la cuenta
     private String IBAN;
+    //titular/res de la cuenta
     private Cliente titular;
+    //saldo disponible en la cuenta
     private double saldo;
+    //indicador de que la cuenta está activa
     private boolean activo;
+    //indicador de que la cuente permite deber saldo al banco
+    private boolean allowCredits;
 
     public Cuenta() {
         this.IBAN = "";
@@ -21,6 +27,10 @@ public class Cuenta {
         this.titular = cliente;
     }
 
+    public void setActiveAccount(boolean activo) {
+        this.activo = activo;
+    }
+
     public void addSaldo(double ingreso) {
         this.saldo += ingreso;
     }
@@ -28,5 +38,27 @@ public class Cuenta {
     public void subSaldo(double restarSaldo) {
         this.saldo -= restarSaldo;
     }
+
+    public boolean getAllowCredit() {
+        return this.allowCredits;
+    }
+
+    public boolean getActiveAccount(){
+        return this.activo;
+    }
+
+    public String getIBAN() {
+        return this.IBAN;
+    }
+
+    public Cliente getClientes() {
+        return this.titular;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+
     
 }
