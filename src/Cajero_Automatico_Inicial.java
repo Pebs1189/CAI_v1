9b067d449f33d18d1a2469191e6f9b144865b9cf
Pablo Barba <pabbarso181189@gmail.com>
2023-02-27 21:36:01 +0100

    Se añaden mas casos

--- src/Cajero_Automatico_Inicial.java
+++ src/Cajero_Automatico_Inicial.java
@@ -14,10 +14,16 @@ public class Cajero_Automatico_Inicial {
         arturo.setCuenta(1, arturoAccount);
         //Añadimos el cliente a la cuenta
         arturoAccount.setTitular(arturo);
+
+        Cajero cajero = new Cajero();
+        cajero.addAccount(1, arturoAccount);
+        cajero.addClient(1,  arturo);
+
         //Declaramos el scanner para poder leer desde teclado
         Scanner tecl = new Scanner(System.in);
         //Variable para seleccionar el menu de opciones
         int menu = -1;
+
         //Bucle para el menú de opciones
         while (menu != 2) {
             System.out.println("Bienvenido a PABS Bank");
@@ -31,7 +37,15 @@ public class Cajero_Automatico_Inicial {
             //opciones del menu
             switch (menu) {
                 case 1:
-                    System.out.println("Se ha pulsado: "+menu);
+                    System.out.println("Introduce el IBAN de la cuenta: ");
+                    String IBAN = tecl.nextLine();
+                    System.out.println("Introduce el PIN");
+                    int PIN = tecl.nextInt(); 
+
+                    /*
+                     * Validar que el IBAN y el PIN es correcto, en ése orden.
+                     */
+                    //TODO
                     break;
                 case 2:
                     System.out.println("Gracias! vuelva pronto.");
