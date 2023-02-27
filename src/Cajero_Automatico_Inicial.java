adf46f10df768bb941340b35a4cb904cb40b7555
Pablo Barba <pabbarso181189@gmail.com>
2023-02-27 21:04:58 +0100

    Añadimos el menú inicial

--- src/Cajero_Automatico_Inicial.java
+++ src/Cajero_Automatico_Inicial.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 import model.Cajero;
 import model.Cliente;
 import model.Cuenta;
@@ -12,7 +14,37 @@ public class Cajero_Automatico_Inicial {
         arturo.setCuenta(1, arturoAccount);
         //Añadimos el cliente a la cuenta
         arturoAccount.setTitular(arturo);
+        //Declaramos el scanner para poder leer desde teclado
+        Scanner tecl = new Scanner(System.in);
+        //Variable para seleccionar el menu de opciones
+        int menu = -1;
+        //Bucle para el menú de opciones
+        while (menu != 2) {
+            System.out.println("Bienvenido a PABS Bank");
+            System.out.println("1. Login");
+            System.out.println("2. Exit");
+            System.out.println("Inicie sesion para realizar cualquier tramite...");
 
-        
+            //Lectura del teclado
+            menu = tecl.nextInt();
+
+            //opciones del menu
+            switch (menu) {
+                case 1:
+                    System.out.println("Se ha pulsado: "+menu);
+                    break;
+                case 2:
+                    System.out.println("Gracias! vuelva pronto.");
+                     //cerramos el scanner
+                    tecl.close();
+                    break;
+                default:
+                    System.out.println("Introduzca un valor correcto.");
+                    break;
+            }
+
+            System.out.println("");
+            System.out.println("");
+        };       
     }
 }
